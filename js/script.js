ca9f1fee902dd8c8c875c9ec893a90ab0f34ae55
Andrii Kulyk <andrij.kulyk@gmail.com>
2023-03-13 12:59:13 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,54 @@
+"use strict"
+
+window.addEventListener('load', onPageLoad);
+
+function onPageLoad() {
+    const buttons = document.querySelectorAll('.button');
+    const about = document.querySelector('.main__about').querySelectorAll('span');
+    const firstTitle = document.querySelector('.first').querySelectorAll('span');
+    const secondTitle = document.querySelector('.second').querySelectorAll('span');
+    const mainImg = document.querySelector('.main__img');
+    const partners = document.querySelector('.partners-wrapper');
+
+    let timeout = 0;
+
+    for (let i = 0; i < firstTitle.length || i < secondTitle.length; i += 1) {
+        timeout += 150;
+        setTimeout(() => {
+            firstTitle[i]?.classList.add('shown');
+            secondTitle[i]?.classList.add('shown');
+        }, timeout);
+    };
+
+    about.forEach(word => {
+        timeout += 150;
+        setTimeout(() => {
+            word.classList.add('shown');
+        }, timeout);
+    });
+
+    mainImg.style.cssText = `animation: img-show 1s linear; animation-delay: ${timeout / 2}ms; animation-fill-mode: forwards;`;
+
+    timeout += 500;
+
+    setTimeout(() => {
+        partners.classList.add('shown');
+    }, timeout);
+
+    buttons.forEach(button => {
+        timeout += 1000;
+        setTimeout(() => {
+            button.classList.add('shown');
+        }, timeout)
+    });
+
+    timeout += 1000;
+
+    setTimeout(() => {
+        buttons.forEach(button => {
+            if (!button.classList.contains('active')) {
+                button.style.cssText = `animation: button-shake 0.8s linear;`;
+            };
+        });
+    }, timeout);
+};
